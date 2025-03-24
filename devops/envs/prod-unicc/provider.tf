46eb3e53b0a10675e63d72685d212f4e8b3e7de3
Sacha Laurent <sacha.laurent@finddx.org>
2025-03-24 17:46:03 +0100

    removing provider update for clarity for the moment

--- devops/envs/prod-unicc/provider.tf
+++ devops/envs/prod-unicc/provider.tf
@@ -1,16 +1,10 @@
 provider "aws" {
   region = local.aws_region
-  default_tags {
-    tags = local.tags
-  }
 
 }
 
 provider "aws" {
   alias  = "useast1"
   region = "us-east-1"
-  default_tags {
-    tags = local.tags
-  }
 
 }
